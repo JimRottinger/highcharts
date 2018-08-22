3686c03396de95cb6002877fa5040eabfe252d46
Jim Rottinger <jamesrottinger@gmail.com>
2018-08-22 11:43:31 -0700

    Submodule the code folder for distribution

--- code/js/modules/overlapping-datalabels.js
+++
@@ -1,8 +0,0 @@
-/*
- Highcharts JS v6.1.1-modified (2018-08-21)
-
- (c) 2009-2017 Torstein Honsi
-
- License: www.highcharts.com/license
-*/
-(function(a){"object"===typeof module&&module.exports?module.exports=a:a(Highcharts)})(function(a){});
